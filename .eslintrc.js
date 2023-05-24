0d22ad2b3e62d1aa2f5fd479ad0af57fc3596ff6
Ivan Brykov <ivan.ru@gmail.com>
2023-05-24 09:38:15 -0500

    initialize the app

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    settings: {
+        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+    plugins: [],
+    extends: [
+        'plugin:@shopify/typescript',
+        'plugin:@shopify/react',
+        'plugin:@shopify/prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            parserOptions: {
+                project: 'tsconfig.json',
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+    ],
+    rules: {
+        '@shopify/no-useless-computed-properties': 'off',
+        '@shopify/jsx-no-hardcoded-content': 'off',
+        '@shopify/strict-component-boundaries': 'off',
+        'no-console': 'warn',
+    },
+};
---
+++ app/javascript/application.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello, Rails 7!</div>;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rootEl = document.getElementById("app");
+  ReactDOM.render(<App />, rootEl);
+});
